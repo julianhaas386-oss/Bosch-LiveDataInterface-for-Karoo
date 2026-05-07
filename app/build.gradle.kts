27af1ef2f8209352bdc827f860e7cf6c7903fff0
julianhaas386-oss <julianhaas386@gmail.com>
2026-05-07 10:04:37 +0200

    chore: bump version to 0.3.0 (Briefing 5 complete)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.dxmedia.bosch.ldi"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
